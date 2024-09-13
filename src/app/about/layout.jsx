5f9b028684feee55ba2345fe4bbdf694de42b0d4
gaiyacaleb <gaiya.caleb30@gmail.com>
2024-09-13 08:56:32 +0100

    fix: modify changes made

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -18,17 +18,17 @@ export default function AboutLayout({children}) {
 
           <h1 className="mx-6"> </h1>
           <p className="mx-6 flex flex-wrap ">
-            <span className="underline ">About Us</span>
+            <span className="underline font-bold">About Us</span>
 
 
 Welcome to BollyG, where your insatiable movie needs are met. Founded in 2019 by Gaiya Caleb, we are dedicated to making your movie experiences second to none.
 
 </p>
-<h3 className="py-5 px-5 underline ">Our Mission</h3>
+<h3 className="py-5 px-5 underline font-bold ">Our Mission</h3>
 <p className="m-2">At BollyG, our mission is to create a platform to express your movie choices. We believe in quencheing that insatiable hunger for movies from your favorite actors. Whether you are looking for series or movies and we are committed to bringing you the most exciting indian shows.</p>
 
           
-            <h3 className="m-5 underline">Our Vision</h3>
+            <h3 className="m-5 underline font-bold">Our Vision</h3>
             <p>BollyG began with a vision to of creating a service for a beautiful service for indians to go watc h newly released movies and also classics . Mr Gaiya has always been saddened with the fact that he would have to go do movie stands everyday just to see a movie and hence came the idea . Over the years, we have grown from a pool of movies  to a big movie outlet, thanks to hardwork of the OG "Mr Gaiya"
             </p>
           </div>

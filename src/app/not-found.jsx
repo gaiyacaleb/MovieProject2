dea6ac73f46ba67500c0e790126a78d2d1987d6b
gaiyacaleb <gaiya.caleb30@gmail.com>
2024-10-14 07:26:45 +0100

    "Updated error messages: changed text color and wording in MoviePage and NotFound components."

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div className='bg-[url(/forestboy.png)]  h-screen flex flex-col items-center justify-center text-white text-4xl uppercase font-bold'>
-        <h1 style={{color:'yellow'}}>No vex, Nothing for you.</h1>
+        <h1 style={{color:'red'}}>No vex, Nothing for you.</h1>
     </div>
   )
 }
